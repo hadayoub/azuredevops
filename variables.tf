dd133e113456d52c20588515fb787cb22f89ebe0
hadayoub <79540094+hadayoub@users.noreply.github.com>
2022-01-17 16:20:21 +0100

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "resourcename" {
+  description = "hadi hia resource groupe"
+}
+variable "location" {
+  description = "hadi hia location "
+}
+variable "storagename" {
+    description = "hadi hia storage name"
+}
+variable "tags" {
+  type = map
+}
+variable "storage_container_name" {
+    description = "hadi hia storage_container_name"
+}
+variable "mydnsname" {
+  type=list
+}
+
+variable "security_rule" {
+
+}
+variable "enviornment" {
+  
+}
